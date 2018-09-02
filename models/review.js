7e3ee3d17a77bd4d0befc91c4a8eb424a3bc4b32
Dacio Romero <dacioromero@gmail.com>
2018-09-01 20:28:05 -0700

    Added vanilla bootstrap

---
+++ models/review.js
@@ -0,0 +1,11 @@
+// models/eview.js
+const mongoose = require('mongoose');
+
+const Review = mongoose.model('Review', {
+    title: String,
+    description: String,
+    movieTitle: String,
+    stars: Number,
+});
+
+module.exports = Review
