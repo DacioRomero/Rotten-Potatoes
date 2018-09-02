77ac6cdbf47f22eb7e07554b1a226feda477b2dd
Dacio Romero <dacioromero@gmail.com>
2018-09-01 20:49:50 -0700

    Remove depreciated code, useNewUrlParser for MongoDB

--- app.js
+++ app.js
@@ -11,80 +11,10 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes');
-
-// // OUR MOCK ARRAY OF PROJECTS
-// let reviews = [
-//     { title: "Great Review" },
-//     { title: "Next Review" },
-//     { title: "Garbage Review" }
-// ]
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 reviews = require("./controllers/reviews")
 reviews(app)
-
-// // INDEX
-// app.get('/', (req, res) => {
-//   Review.find()
-//     .then(reviews => {
-//       res.render('reviews-index', { reviews: reviews });
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// });
-
-// // NEW
-// app.get('/reviews/new', (req, res) => {
-//   res.render('reviews-new', {});
-// });
-//
-// // CREATE
-// app.post('/reviews', (req, res) => {
-//     Review.create(req.body).then((review) => {
-//         console.log(review)
-//         res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
-//     }).catch((err) => {
-//         console.log(err.message)
-//     });
-// });
-//
-// // SHOW
-// app.get('/reviews/:id', (req, res) => {
-//     Review.findById(req.params.id).then((review) => {
-//         res.render('reviews-show', { review: review })
-//     }).catch((err) => {
-//         console.log(err.message);
-//     });
-// });
-//
-// // EDIT
-// app.get('/reviews/:id/edit', function (req, res) {
-//     Review.findById(req.params.id, function(err, review) {
-//         res.render('reviews-edit', {review: review});
-//     });
-// });
-//
-// // UPDATE
-// app.put('/reviews/:id', (req, res) => {
-//     Review.findByIdAndUpdate(req.params.id, req.body)
-//         .then(review => {
-//             res.redirect(`/reviews/${review._id}`)
-//         })
-//         .catch(err => {
-//             console.log(err.message)
-//         });
-// });
-//
-// // DELETE
-// app.delete('/reviews/:id', function (req, res) {
-//     console.log("DELETE review")
-//     Review.findByIdAndRemove(req.params.id).then((review) => {
-//         res.redirect('/');
-//     }).catch((err) => {
-//         console.log(err.message);
-//     });
-// });
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
