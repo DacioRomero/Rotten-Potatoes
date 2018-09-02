f41ef51a76516a291faa4b23d428b1b3f2bee514
Dacio Romero <dacioromero@gmail.com>
2018-09-01 20:59:31 -0700

    Attempt to fix Heroku crash

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
 reviews = require("./controllers/reviews")
 reviews(app)
