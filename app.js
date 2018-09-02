c1d08efee7f1824230124c74a1dc204f8c36d319
Dacio Romero <dacioromero@gmail.com>
2018-09-01 17:18:01 -0700

    Increase indentation, add new "review"

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ app.set('view engine', 'handlebars');
 
 // OUR MOCK ARRAY OF PROJECTS
 let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" }
+    { title: "Great Review" },
+    { title: "Next Review" },
+    { title: "Garbage Review" }
 ]
 
 // INDEX
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+    res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(3000, () => {
