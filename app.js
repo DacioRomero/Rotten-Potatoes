b54ca795fd2620558904996d5da38b97aa0a46d2
Dacio Romero <dacioromero@gmail.com>
2018-09-01 18:36:45 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // app.js
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
 
 const Review = mongoose.model('Review', {
     title: String,
-})
+    description: String,
+    movieTitle: String,
+    stars: Number,
+});
 
 // OUR MOCK ARRAY OF PROJECTS
 // let reviews = [
@@ -27,9 +33,33 @@ app.get('/', (req, res) => {
     })
     .catch(err => {
       console.log(err);
-    })
+    });
+});
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+});
+
+// CREATE
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+        console.log(review)
+        res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+    }).catch((err) => {
+        console.log(err.message)
+    });
+});
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+    Review.findById(req.params.id).then((review) => {
+        res.render('reviews-show', { review: review })
+    }).catch((err) => {
+        console.log(err.message);
+    });
 });
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
-})
+});
