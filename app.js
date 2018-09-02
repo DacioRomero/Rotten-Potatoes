920671e80087fb7e9adfdc3f4d2077ff6bb23817
Dacio Romero <dacioromero@gmail.com>
2018-09-01 17:54:50 -0700

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 // app.js
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 var exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useMongoClient: true });
+
+const Review = mongoose.model('Review', {
+    title: String,
+})
 
 // OUR MOCK ARRAY OF PROJECTS
-let reviews = [
-    { title: "Great Review" },
-    { title: "Next Review" },
-    { title: "Garbage Review" }
-]
+// let reviews = [
+//     { title: "Great Review" },
+//     { title: "Next Review" },
+//     { title: "Garbage Review" }
+// ]
 
 // INDEX
 app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
-})
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!')
