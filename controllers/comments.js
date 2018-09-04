8c9fb9ddfaa346c4d99402d2e0239f16f68ffb80
Dacio Romero <dacioromero@gmail.com>
2018-09-04 15:43:53 -0700

    Reformat, remove nodemon, update .gitignore

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,25 +1,26 @@
 // controllers/comments.js
 
-const Comment = require('../models/comment')
+const Comment = require('../models/comment');
 
 function commentsController (app) {
-    // CREATE Comment
+    // CREATE
     app.post('/reviews/comments', (req, res) => {
-        Comment.create(req.body).then(comment => {
-            res.redirect(`/reviews/${comment.reviewId}`);
-        }).catch((err) => {
-            console.log(err.message);
-        });
+        Comment.create(req.body)
+            .then(comment => {
+                res.redirect(`/reviews/${comment.reviewId}`);
+            }).catch((err) => {
+                console.log(err.message);
+            });
     });
 
     // DELETE
     app.delete('/reviews/comments/:id', function (req, res) {
-        console.log("DELETE comment")
-        Comment.findByIdAndRemove(req.params.id).then((comment) => {
-            res.redirect(`/reviews/${comment.reviewId}`);
-        }).catch((err) => {
-            console.log(err.message);
-        });
+        Comment.findByIdAndRemove(req.params.id)
+            .then((comment) => {
+                res.redirect(`/reviews/${comment.reviewId}`);
+            }).catch((err) => {
+                console.log(err.message);
+            });
     });
 }
 
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,49 +1,51 @@
 // controllers/reviews.js
 
-const Review = require('../models/review')
-const Comment = require('../models/comment')
+const Review = require('../models/review');
+const Comment = require('../models/comment');
 
 function reviewsController (app) {
 
   app.get('/', (req, res) => {
-    Review.find()
-      .then(reviews => {
-        res.render('reviews-index', {reviews: reviews});
-      })
-      .catch(err => {
-        console.log(err);
-      });
+      Review.find()
+          .then(reviews => {
+              res.render('reviews-index', {reviews: reviews});
+          })
+          .catch(err => {
+              console.log(err);
+          });
   });
 
   // NEW
   app.get('/reviews/new', (req, res) => {
-    res.render('reviews-new', {});
+      res.render('reviews-new', {});
   });
 
   // CREATE
   app.post('/reviews', (req, res) => {
-      Review.create(req.body).then((review) => {
-          res.redirect(`/reviews/${review._id}`)
-      }).catch((err) => {
-          console.log(err.message)
-      });
+      Review.create(req.body)
+          .then((review) => {
+              res.redirect(`/reviews/${review._id}`);
+          }).catch((err) => {
+              console.log(err.message);
+          });
   });
 
   // SHOW
   app.get('/reviews/:id', (req, res) => {
-      Review.findById(req.params.id).then(review => {
-          Comment.find({ reviewId: req.params.id }).then(comments => {
-              res.render('reviews-show', { review: review, comments: comments });
-          })
-      }).catch((err) => {
-          console.log(err.message);
-      });
+      Review.findById(req.params.id)
+          .then(review => {
+              Comment.find({ reviewId: req.params.id }).then(comments => {
+                  res.render('reviews-show', { review: review, comments: comments });
+              })
+          }).catch((err) => {
+              console.log(err.message);
+          });
   });
 
   // EDIT
   app.get('/reviews/:id/edit', function (req, res) {
-      Review.findById(req.params.id, function(err, review) {
-          res.render('reviews-edit', {review: review});
+      Review.findById(req.params.id, (err, review) => {
+          res.render('reviews-edit', { review: review });
       });
   });
 
@@ -51,21 +53,22 @@ function reviewsController (app) {
   app.put('/reviews/:id', (req, res) => {
       Review.findByIdAndUpdate(req.params.id, req.body)
           .then(review => {
-              res.redirect(`/reviews/${review._id}`)
+              res.redirect(`/reviews/${review._id}`);
           })
           .catch(err => {
-              console.log(err.message)
+              console.log(err.message);
           });
   });
 
   // DELETE
   app.delete('/reviews/:id', function (req, res) {
       console.log("DELETE review")
-      Review.findByIdAndRemove(req.params.id).then((review) => {
-          res.redirect('/');
-      }).catch((err) => {
-          console.log(err.message);
-      });
+      Review.findByIdAndRemove(req.params.id)
+          .then((review) => {
+              res.redirect('/');
+          }).catch((err) => {
+              console.log(err.message);
+          });
   });
 
 }
