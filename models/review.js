9c3426b53b4fc72d8e570412982db695d6c6e3e1
Dacio Romero <dacioromero@gmail.com>
2018-09-12 14:52:29 -0700

    Link reviews to movies

--- models/review.js
+++ models/review.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 
 const Review = mongoose.model('Review', {
+    movieId: { type: String, required: true },
     title: String,
     description: String,
     movieTitle: String,
