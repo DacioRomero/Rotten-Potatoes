71db05b02460b57a73abc4bd832930ec62dcfe14
Dacio Romero <dacioromero@gmail.com>
2018-09-12 14:52:29 -0700

    Link reviews to movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,6 +1,7 @@
 // movies.js
 const MovieDb = require('moviedb-promise');
 const moviedb = new MovieDb('1f11d7564e8cf70631edd81979b7ae77');
+const Review = require('../models/review')
 
 var genres = {};
 moviedb.genreMovieList().then(response => {
@@ -24,7 +25,10 @@ function moviesController(app){
             moviedb.movieVideos({ id: req.params.id })
             .then(videos => {
                 movie.trailer_youtube_id = videos.results[0].key
-                res.render('movies-show', { movie: movie });
+                Review.find({ movieId: req.params.id })
+                .then(reviews => {
+                    res.render('movies-show', { movie: movie, reviews: reviews });
+                })
             })
             .catch(console.error)
         })
