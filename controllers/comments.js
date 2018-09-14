ee309cbed1791256f1a09a6bbc2e4c2b53694a89
Dacio Romero <dacioromero@gmail.com>
2018-09-14 10:09:40 -0700

    Delete comments with AJAX

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,24 +5,24 @@ const Comment = require('../models/comment');
 function commentsController (app) {
     // CREATE
     app.post('/reviews/comments', (req, res) => {
-        console.log(req.body)
         Comment.create(req.body)
-            .then(comment => {
-                res.status(200).send({ comment: comment });
-            }).catch((err) => {
-                res.status(400).send({ err: err });
-            });
+        .then(comment => {
+            res.status(200).send({ comment: comment });
+        }).catch((err) => {
+            res.status(400).send({ err: err });
+        });
     });
 
     // DELETE
     app.delete('/reviews/comments/:id', function (req, res) {
-        Comment.findByIdAndRemove(req.params.id)
-            .then((comment) => {
-                res.redirect(`/reviews/${comment.reviewId}`);
-            }).catch((err) => {
-                console.log(err.message);
-            });
-    });
+        console.log("DELETE comment")
+        Comment.findByIdAndRemove(req.params.id).then(comment => {
+            res.status(200).send(comment);
+        }).catch((err) => {
+            console.log(err.message);
+            res.status(400).send(err)
+        })
+    })
 }
 
 module.exports = commentsController;
